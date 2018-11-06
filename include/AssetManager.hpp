12c6be9963b879c93f92dcc8ca5ac6098a4f9c1c
Nuri Uzunoğlu <nuriu@protonmail.com>
2018-11-06 19:26:02 +0300

    added sound functions to asset manager.

--- include/AssetManager.hpp
+++ include/AssetManager.hpp
@@ -4,22 +4,32 @@
 
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
 
 #include <string>
+#include <memory>
 #include <map>
 
 class AssetManager
 {
   public:
     void loadFont(const std::string& name, const std::string& path);
+    void loadSound(const std::string& name, const std::string& path);
     void loadTexture(const std::string& name, const std::string& path);
+
+    void playSound(const std::string& name);
+    void pauseSound(const std::string& name);
+    void stopSound(const std::string& name);
 
     sf::Font& getFont(const std::string& name);
     sf::Texture& getTexture(const std::string& name);
 
   private:
     std::map<std::string, sf::Font> m_Fonts;
+    std::map<std::string, sf::Sound> m_Sounds;
     std::map<std::string, sf::Texture> m_Textures;
+    std::map<std::string, sf::SoundBuffer> m_SoundBuffers;
 };
 
 #endif // ASSET_MANAGER_HPP
--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -2,22 +2,48 @@
 
 void AssetManager::loadFont(const std::string& name, const std::string& path)
 {
-    sf::Font font;
+    auto font = std::make_unique<sf::Font>();
 
-    if (font.loadFromFile(path))
+    if (font->loadFromFile(path))
     {
-        m_Fonts[name] = font;
+        m_Fonts[name] = *font;
+    }
+}
+
+void AssetManager::loadSound(const std::string& name, const std::string& path)
+{
+    auto buffer = std::make_unique<sf::SoundBuffer>();
+
+    if (buffer->loadFromFile(path))
+    {
+        m_SoundBuffers[name] = *buffer;
+        m_Sounds[name] = sf::Sound(m_SoundBuffers[name]);
     }
 }
 
 void AssetManager::loadTexture(const std::string& name, const std::string& path)
 {
-    sf::Texture texture;
+    auto texture = std::make_unique<sf::Texture>();
 
-    if (texture.loadFromFile(path))
+    if (texture->loadFromFile(path))
     {
-        m_Textures[name] = texture;
+        m_Textures[name] = *texture;
     }
+}
+
+void AssetManager::playSound(const std::string& name)
+{
+    m_Sounds[name].play();
+}
+
+void AssetManager::pauseSound(const std::string& name)
+{
+    m_Sounds[name].pause();
+}
+
+void AssetManager::stopSound(const std::string& name)
+{
+    m_Sounds[name].stop();
 }
 
 sf::Font& AssetManager::getFont(const std::string& name)
