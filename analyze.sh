6e2d9efb0561dc3f399ee9185bbf6693a37dd372
Nuri Uzunoğlu <nuriu@protonmail.com>
2018-11-22 14:22:49 +0300

    updated analyze.sh.
    - now if file arg didn't sent it only runs on changed files instead of all files.
    - now it uses .clang-tidy config file for clang-tidy operations.
    - now it runs clang-format on file(s) automatically before analyzing operations.

--- analyze.sh
+++ analyze.sh
@@ -1,44 +1,36 @@
 #!/usr/bin/env bash
 file=''
-
-print_usage() {
-  printf "\nanalyze.sh is responsible from running static code analyses.\n"
-  printf "\n\tUsage: \n"
-  printf "\t\tanalyze.sh -f <file-to-analyze>\n"
-}
+changed_files=$(git diff --name-only HEAD | grep -E '.*\.(hpp|cpp)$' | cut -f 2)
 
 while getopts 'f:v' flag; do
   case "${flag}" in
-    f) file="${OPTARG}" ;;
-    *) print_usage
+    f) file="${OPTARG}"
     exit 1 ;;
   esac
 done
 
 if [[ -n ${file} ]]; then
-  echo ""
-  printf "clang-tidy results:\n\n"
+  clang-format -style=file -i ${file}
 
-  clang-tidy $file -checks=* -- -I include/
+  echo ""
+  printf "\ncppcheck results:\n\n"
+  cppcheck ${file} -I include/ -I /usr/include/ -I lib/spdlog/include/ -I lib/imgui/ -I lib/imgui-sfml/ -I lib/tmxlite/tmxlite/include/ --enable=all --inconclusive --quiet
 
   echo ""
   echo "-----------------------"
-
-  printf "\ncppcheck results:\n\n"
-
-  cppcheck $file -I include/ --enable=all --inconclusive --quiet
+  printf "clang-tidy results:\n\n"
+  clang-tidy ${file} -checks=* -- -I include/ -I /usr/include/ -I lib/spdlog/include/ -I lib/imgui/ -I lib/imgui-sfml/ -I lib/tmxlite/tmxlite/include/
 else
-  echo ""
-  printf "clang-tidy results:\n\n"
+  clang-format -style=file -i ${changed_files}
 
-  clang-tidy src/* -checks=bugprone-*,cert-*,cppcoreguidelines-*,clang-analyzer-*,hicpp-*,misc-*,modernize-*,performance-*,portability-*,readability-* -- -I include/
+  echo ""
+  printf "\ncppcheck results:\n\n"
+  cppcheck ${changed_files} -I include/ -I lib/spdlog/include/ -I lib/imgui/ -I lib/imgui-sfml/ -I lib/tmxlite/tmxlite/include/ --enable=all --inconclusive --quiet --force
 
   echo ""
   echo "-----------------------"
-
-  printf "\ncppcheck results:\n\n"
-
-  cppcheck src/* -I include/ --enable=all --inconclusive --quiet
+  printf "clang-tidy results:\n\n"
+  clang-tidy ${changed_files} -- -I include/ -I /usr/include/ -I lib/spdlog/include/ -I lib/imgui/ -I lib/imgui-sfml/ -I lib/tmxlite/tmxlite/include/
 fi
 
 printf "\n"
